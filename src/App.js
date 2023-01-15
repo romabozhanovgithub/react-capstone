452f45fcb74ca3d1d5b3d247c77e84782a2be3f2
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-16 01:31:08 +0300

    add react-router and routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <Directory categories={categories} />
+    <Routes>
+      {/*
+        For nested routes:
+          <Route path="/" element={<Home />}>
+            <Route path="about" element={<About />} />
+            <Route path="events" element={<Events />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        And in the component:
+          <Outlet />
+
+        And for dynamic routes:
+          <Route path="/" element={<Home />}>
+            <Route path=":id" element={<Event />} />
+          </Route>
+      */}
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
