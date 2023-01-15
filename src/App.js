fd6ef18e05b89ee72553d2dc67b0955081404f6a
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-15 23:26:57 +0300

    add category-item and directory components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      linkUrl: "shop/hats",
     },
     {
       id: 2,
       title: "Jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      linkUrl: "shop/jackets",
     },
     {
       id: 3,
       title: "Sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      linkUrl: "shop/sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
-    <div className="categories-container">
-      {
-        categories.map(({ id, title, imageUrl, linkUrl }) => (
-          <div className="category-container" key={id}>
-            <div className="background-image" />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Show Now</p>
-            </div>
-          </div>
-        ))
-      }
-    </div>
+    <Directory categories={categories} />
   );
 }
 
